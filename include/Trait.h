acb433a4438a9376bf40e47cb59a7e45198058be
Patrick Putnam <patrick.putnam@cchmc.org>
2013-08-09 16:14:07 -0400

    Added Trait class

--- include/Trait.h
+++ include/Trait.h
@@ -26,19 +26,40 @@
  * of the authors and should not be interpreted as representing official policies,
  * either expressed or implied, of the FreeBSD Project.
  ******************************************************************************/
+#ifndef TRAIT_H_
+#define TRAIT_H_
 
-#ifndef MARKER_H_
-#define MARKER_H_
+#include "common.h"
 
 /*******************************************************************************
- * A Marker is a genomic element of interest.
+ * A Trait is some observed characteristic of an individual.
+ *
+ * A set of Traits is used to define a Phenotype.
+ *
+ * There are two types of traits: categorical or quantitative.
+ *
+ * Quantitative traits are represented by a measured value or quantity. For 
+ * example, height is considered to be a quantitative value.
+ *
+ * Qualitative traits fall into a general set of values. For example, eye color
+ * is a categorical trait, and is limited to a set of colors.
+ *
  ******************************************************************************/
-class Marker {
+
+class Trait {
 public:
-    Marker();
-    virtual Marker();
+    String getName() const;
+    String getDescription() const;
+
+    template < class P >
+    P & getProperty( const String & key ) const;
+
+    template < typename AggType, typename ResType >
+    ResType aggregate( AggType agg );
+
 private:
-    
+    class Property;
+    boost::scoped_ptr< Property > m_prop;
 };
 
-#endif  // MARKER_H_
+#endif  // TRAIT_H_
