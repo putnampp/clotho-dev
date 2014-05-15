7a32a31b99b1c62099aff48ef2348fa5d4dea820
Patrick Putnam <putnampp@gmail.com>
2014-05-15 11:20:04 -0400

    Opted to use a larger header structure to obtain direct pointers to blocked regions. Goal is to simplify downstream pointer jumping algorithms

--- include/engine/memory_page.h
+++ include/engine/memory_page.h
@@ -5,17 +5,23 @@
 
 typedef unsigned short offset_t;
 
+//struct memory_page_header {
+//    offset_t free_start, free_end;
+//
+//    memory_page_header( offset_t s_off, offset_t e_off ) :
+//        free_start( s_off ),
+//        free_end( e_off )
+//    {}
+//
+//    memory_page_header( offset_t size ) : 
+//        memory_page_header(0, size )
+//    {}
+//};
+
 struct memory_page_header {
-    offset_t free_start, free_end;
+    char * free_start, * free_end;
 
-    memory_page_header( offset_t s_off, offset_t e_off ) :
-        free_start( s_off ),
-        free_end( e_off )
-    {}
-
-    memory_page_header( offset_t size ) : 
-        memory_page_header(0, size )
-    {}
+    memory_page_header( char * s = NULL, char * e = NULL ) : free_start(s), free_end(e) {}
 };
 
 static const unsigned short BLOCK_SIZE = PAGE_SIZE - sizeof( memory_page_header ) - sizeof( void * );
@@ -26,7 +32,12 @@ struct memory_page {
 
     char block[ BLOCK_SIZE ];
 
-    memory_page( memory_page * n = NULL) : next(n), header( 0, BLOCK_SIZE ) {}
+//    memory_page( memory_page * n = NULL) : next(n), header( 0, BLOCK_SIZE ) {}
+    memory_page( memory_page * n = NULL ) : next(n), header(block, block + BLOCK_SIZE) {}
+    void clear() { 
+        header.free_start = block; 
+        header.free_end = block + BLOCK_SIZE;  
+    }
 };
 
 #endif  // MEMORY_PAGE_H_
