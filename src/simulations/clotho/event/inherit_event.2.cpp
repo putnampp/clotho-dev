e943312184bd019427135f6ca0c3f5d9840fa4f4
Patrick Putnam <putnampp@gmail.com>
2014-05-29 08:00:04 -0400

    g++ seems to complain about being unable to compute the offsetof of the templated version of this event. Therefore generalized event to simply maintain a void pointer to the gamete.

---
+++ src/simulations/clotho/event/inherit_event.2.cpp
@@ -0,0 +1,3 @@
+#include "inherit_event.2.h"
+
+typename InheritEvent::object_manager_t InheritEvent::m_pool;
---
+++ src/simulations/clotho/event/inherit_event.2.h
@@ -0,0 +1,78 @@
+#ifndef INHERIT_EVENT_HPP_
+#define INHERIT_EVENT_HPP_
+
+#include "../clotho_event.h"
+
+class InheritEvent : public ClothoEvent {
+public:
+
+    static const event_type_t TYPE_ID = 4;
+    typedef void * gamete_ptr_t;
+
+    typedef ObjectManager< InheritEvent, EVENT_PAGE_SIZE > object_manager_t;
+
+    // by default gamete index is unknown (-1)
+    // gamete index can be used to identify which "source" ("parent")
+    // a gamete was inherited from (ie. source gender)
+    void init( const vtime_t & tSent, const vtime_t & tRecv,
+                const system_object * sender, const system_object * receiver,
+                event_id_t eid, gamete_ptr_t  z, unsigned char gidx = -1) {
+        ClothoEvent::init( tSent, tRecv, sender, receiver, eid );
+        m_gidx = gidx;
+        m_gamete = z;
+    }
+
+    void init( const vtime_t & tSent, const vtime_t & tRecv,
+                const system_id & sender, const system_id & receiver,
+                event_id_t eid, gamete_ptr_t z, unsigned char gidx = -1) {
+        ClothoEvent::init( tSent, tRecv, sender, receiver, eid );
+        m_gidx = gidx;
+        m_gamete = z;
+    }
+
+    inline event_type_t getEventType() const { return TYPE_ID; }
+
+    unsigned char getParentIndex() const { return m_gidx; }
+    gamete_ptr_t getGamete() const { return m_gamete; }
+
+    static void * operator new( size_t s ) {
+        void * res = m_pool.malloc();
+        return res;
+    }
+
+    static void operator delete( void * ptr ) {
+        m_pool.free( (InheritEvent *) ptr );
+    }
+
+    virtual ~InheritEvent() {}
+//protected:
+
+    InheritEvent() {}
+
+    // by default gamete index is unknown (-1)
+    // gamete index can be used to identify which "source" ("parent")
+    // a gamete was inherited from (ie. source gender)
+    InheritEvent( const vtime_t & tSent, const vtime_t & tRecv,
+                const system_object * sender, const system_object * receiver,
+                event_id_t eid, gamete_ptr_t z, unsigned char gidx = -1) :
+        ClothoEvent( tSent, tRecv, sender, receiver, eid ),
+        m_gidx( gidx ),
+        m_gamete( z )
+    {}
+
+    InheritEvent( const vtime_t & tSent, const vtime_t & tRecv,
+                const system_id & sender, const system_id & receiver,
+                event_id_t eid, gamete_ptr_t z, unsigned char gidx = -1) :
+        ClothoEvent( tSent, tRecv, sender, receiver, eid ),
+        m_gidx( gidx ),
+        m_gamete( z )
+    {}
+
+protected:
+    unsigned char   m_gidx;
+    gamete_ptr_t        m_gamete;
+
+    static object_manager_t m_pool;
+};
+
+#endif  // INHERIT_EVENT_HPP_
