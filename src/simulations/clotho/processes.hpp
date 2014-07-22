f1994dffa1e6f39b5060269325268ee3f2acbb34
Patrick Putnam <putnampp@gmail.com>
2014-07-22 17:48:23 -0400

    Moved mutation and recombination from models to processes

---
+++ src/simulations/clotho/processes.hpp
@@ -0,0 +1,6 @@
+#ifndef PROCESSES_GUARD_HPP_
+#define PROCESSES_GUARD_HPP_
+
+#include "processes/processes.hpp"
+
+#endif  // PROCESSES_GUARD_HPP_
---
+++ src/simulations/clotho/processes/processes.hpp
@@ -0,0 +1,6 @@
+#ifndef PROCESSES_HPP_
+#define PROCESSES_HPP_
+
+#include "processes/config.hpp"
+
+#endif  // PROCESSES_HPP_
