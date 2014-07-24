bca56516ac08d4bf8019df0e6a3967927bc32fe1
Patrick Putnam <putnampp@gmail.com>
2014-07-24 09:21:37 -0400

    Cleaned up commented out sections of code

--- src/genetics/allele_alphabet.cpp
+++ src/genetics/allele_alphabet.cpp
@@ -7,8 +7,6 @@
 AlleleAlphabet::AlleleAlphabet() {}
 
 AlleleAlphabet::index_type     AlleleAlphabet::getSymbol( const locus_t & l, const allele_t & a, bool createNew ) {
-//    std::cout << "Getting symbol for: " << l << std::endl;
-
     locus_iterator it = m_db.end();
     allele_iterator a_it = m_alleles.find(a);
 
@@ -43,8 +41,6 @@ AlleleAlphabet::index_type     AlleleAlphabet::getSymbol( const locus_t & l, con
     if( offset == bitset_type::npos ) {
         // inactive variant
         //
-//        std::cout << "Free list size: " << m_free_list.size() << std::endl;
-//        std::cout << "count size: " << m_free_list.count() << std::endl;
         offset = m_free_list.find_first();
         if( offset == bitset_type::npos ) {
             offset = m_active.size();
@@ -53,86 +49,31 @@ AlleleAlphabet::index_type     AlleleAlphabet::getSymbol( const locus_t & l, con
             m_free_intersect.push_back(false);
             m_free_union.push_back(false);
         } else {
-//            std::cout << "Active size: " << m_active.size() << std::endl;
             assert( offset < m_active.size() );
-//            std::cout << "Reusing offset: " << offset << std::endl;
-//            m_active[ offset ]->second.second = bitset_type::npos;
             m_db.erase( m_active[offset] );
             m_active[offset] = it;
             m_free_list[ offset ] = false;
         }
         it->second.second = offset;
-//        std::cout << "count size: " << m_free_list.count() << std::endl;
-//        std::cout << "Free list size: " << m_free_list.size() << std::endl;
     }
-//    std::cout << "Offset: " << offset << std::endl;
     return offset;
 }
 
 void AlleleAlphabet::updateFreeSymbols( const bitset_type & fs ) {
     // boost::dynamic_bitset returns # of bits as size
-//    if( fs.num_blocks() > m_free_list.num_blocks() ) {
-//        size_t max = ((m_free_intersect.size() >= m_free_union.size() ) ? m_free_intersect.size() : m_free_union.size());
-//        max = ((max >= m_free_list.size()) ? max : m_free_list.size());
-//        max = ((max >= fs.size()) ? max : fs.size());
-
-//        assert( max <= fs.size());
-//        size_t max = fs.num_blocks() - m_free_list.num_blocks();
-//        while( max-- ) {
-//            m_free_list.append( 0 );
-//            m_free_intersect.append(0);
-//            m_free_union.append(0);
-//        }
-//    }
-
-    
-//    typedef bitset_type::block_type block_type;
-
-//    std::cout << "FS: " << fs.size() << std::endl;
-//    std::cout << "FL: " << m_free_list.size() 
-//                << "; FI: " << m_free_intersect.size()
-//                << "; FU: " << m_free_union.size() << std::endl;
     assert((m_free_list.size() == m_free_intersect.size()) && (m_free_list.size() == m_free_union.size()));
     bitset_type b(fs);
-//    while( b.size() < m_free_list.size() ) {
-//        b.push_back(false);
-//    }
     if( b.size() < m_free_list.size() ) { b.resize( m_free_list.size(), false ); }
 
-//    std::cout << "B: " << b.size() << std::endl;
-//    assert( b.size() == m_free_intersect.size() );
     m_free_intersect &= b;
-//    assert( b.size() == m_free_union.size());
     m_free_union |= b;
     m_free_list = m_free_intersect | ~m_free_union;
-//    block_type * res = &m_free_list.m_bits[0], 
-//               * inter = &m_free_intersect.m_bits[0], 
-//               * uni = &m_free_union.m_bits[0];
-//
-//    const block_type * next = &fs.m_bits[0];
-//
-//    //size_t i = m_free_list.num_blocks();
-//    size_t i = fs.num_blocks();
-//    while( i-- ) {
-//        block_type b = (*next++);
-//        (*inter) &= b;
-//        (*res) = (*inter++);
-//        (*uni)   |= b;
-//        (*res++) |= ~(*uni++);
-//    }
 }
 
 void AlleleAlphabet::resetFreeSymbols() {
     m_free_list.reset();
     m_free_union.reset();
-    m_free_intersect.reset();
-    m_free_intersect.flip();
-//    typedef bitset_type::block_type block_type;
-//    block_type * p = &m_free_intersect.m_bits[0];
-//    size_t i = m_free_intersect.num_blocks();
-//    while( i-- ) {
-//        (*p++) = (block_type)-1;
-//    }
+    m_free_intersect.reset().flip();
 }
 
 size_t AlleleAlphabet::block_count() {
