f1cff2fa0242941c22b913634c720f15ebbc2753
Patrick Putnam <putnampp@gmail.com>
2014-07-28 17:19:34 -0400

    Cleaned up source files

--- experiments/set_operations/include/union_operation.hpp
+++ experiments/set_operations/include/union_operation.hpp
@@ -4,43 +4,6 @@
 #include <algorithm>
 #include <string>
 
-//template < class SIterator, class SetType, class ResultType = SetType >
-//struct union_operation {
-//    typedef SetType  set_type;
-//    typedef ResultType  result_type;
-//    typedef SIterator   sample_iterator;
-//
-//    void operator()( std::pair< sample_iterator, sample_iterator > paired, result_type & res ) {
-//        typename set_type::element_type::iterator b1 = (*paired.first)->begin(),
-//                                                  e1 = (*paired.first)->end(),
-//                                                  b2 = (*paired.second)->begin(),
-//                                                  e2 = (*paired.second)->end();
-//        std::set_union( b1, e1, b2, e2, std::back_inserter( *res ) );
-//    }
-//
-//    std::string name() {
-//        static const std::string n = "union";
-//        return n;
-//    }
-//};
-
-//template < class SIterator, class V >
-//struct union_operation< SIterator, std::shared_ptr< active_space_subset<V> >, std::shared_ptr< active_space_subset<V> > > {
-//    typedef std::shared_ptr< active_space_subset<V> >  set_type;
-//    typedef std::shared_ptr< active_space_subset<V> > result_type;
-//    typedef SIterator   sample_iterator;
-//
-//    void operator()( std::pair< sample_iterator, sample_iterator > paired, result_type & res ) {
-//        (*res) = (*(*paired.first));
-//        (*res) |= (*(*paired.second));
-//    }
-//
-//    std::string name() {
-//        static const std::string n = "union";
-//        return n;
-//    }
-//};
-
 template < class SetType, class ResultType = SetType >
 struct union_operation {
     typedef SetType set_type;
