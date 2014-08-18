b101ca0cb179074268378f81110f665875945180
Patrick Putnam <putnampp@gmail.com>
2014-08-18 11:28:39 -0400

    Forgot to add accessor

---
+++ include/genetics/accessor.hpp
@@ -0,0 +1,11 @@
+#ifndef ACCESSOR_HPP_
+#define ACCESSOR_HPP_
+
+namespace accessor {
+
+template < class RecordIterator, class Result >
+static Result get( RecordIterator it );
+
+}   // namespace accessor
+
+#endif  // ACCESSOR_HPP_
