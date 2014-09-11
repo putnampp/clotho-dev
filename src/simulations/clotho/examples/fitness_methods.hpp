a5319fbd441eb460bf60203e15a022dfbc812451
Patrick Putnam <putnampp@gmail.com>
2014-09-11 16:20:26 -0400

    reverted back to using the original method of computing fitness

--- src/simulations/clotho/examples/fitness_methods.hpp
+++ src/simulations/clotho/examples/fitness_methods.hpp
@@ -1,8 +1,8 @@
 #ifndef FITNESS_METHODS_HPP_
 #define FITNESS_METHODS_HPP_
 
-//#include "genetics/fitness_bitset.hpp"
-#include "genetics/fitness_bitset2.hpp"
+#include "genetics/fitness_bitset.hpp"
+//#include "genetics/fitness_bitset2.hpp"
 
 template < class Allele >
 struct het_fitness {
@@ -57,14 +57,13 @@ public:
 
         typename gamete_type::alphabet_t::pointer alpha = g1->getAlphabet();
 
-        //fitness_bitset< typename gamete_type::bitset_type::block_type, typename gamete_type::bitset_type::allocator_type, typename gamete_type::alphabet_t, hom_policy, het_policy, double > fit( g1->getBits(), alpha, m_hom_case, m_het_case, f);
-//        boost::to_block_range( *g2->getBits(), fit );
-        fitness_bitset2< typename gamete_type::alphabet_t, hom_policy, het_policy, double > fit( alpha, m_hom_case, m_het_case, f);
+        fitness_bitset< typename gamete_type::bitset_type::block_type, typename gamete_type::bitset_type::allocator_type, typename gamete_type::alphabet_t, hom_policy, het_policy, double > fit( g1->getBits(), alpha, m_hom_case, m_het_case, f);
+        boost::to_block_range( *g2->getBits(), fit );
+        return fit.getResult();
 
-        fit( *g1, *g2, f);
-
-        //return fit.getResult();
-        return f;
+        //fitness_bitset2< typename gamete_type::alphabet_t, hom_policy, het_policy, double > fit( alpha, m_hom_case, m_het_case, f);
+        //fit( *g1, *g2, f);
+        //return f;
     }
 
     virtual ~fitness_multiplicative() {}
